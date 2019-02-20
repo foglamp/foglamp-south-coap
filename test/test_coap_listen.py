0a7f9b1744427120454519d8e74f5d3434db8165
Amarendra <amarendra@dianomic.com>
2019-02-20 13:46:39 +0530

    FOGL-2461 Tests fixed (#15)
    * Tests fixed
    
    * refactored tests and removed debug logs

--- test/test_coap_listen.py
+++ test/test_coap_listen.py
@@ -10,14 +10,14 @@ import asyncio
 import copy
 import json
 import pytest
-from unittest.mock import call, patch
-
 import cbor2
 import aiocoap.error
 from aiocoap import message, numbers
+from unittest.mock import call, patch
 
 from python.foglamp.plugins.south.coap import coap
-from python.foglamp.plugins.south.coap.coap import CoAPIngest, Ingest, _DEFAULT_CONFIG as config
+from python.foglamp.plugins.south.coap.coap import CoAPIngest, async_ingest, c_callback, c_ingest_ref, _DEFAULT_CONFIG as config
+
 
 __author__ = "Amarendra K Sinha"
 __copyright__ = "Copyright (c) 2017 OSIsoft, LLC"
@@ -52,13 +52,15 @@ def test_plugin_contract():
     assert callable(getattr(coap, 'plugin_shutdown'))
     assert callable(getattr(coap, 'plugin_reconfigure'))
 
+    assert callable(getattr(coap, 'plugin_register_ingest'))
+
 
 @pytest.allure.feature("unit")
 @pytest.allure.story("plugin", "south", "coap")
 def test_plugin_info():
     assert coap.plugin_info() == {
         'name': 'CoAP Plugin',
-        'version': '1.0',
+        'version': '1.5.0',
         'mode': 'async',
         'type': 'south',
         'interface': '1.0',
@@ -68,14 +70,14 @@ def test_plugin_info():
 
 @pytest.allure.feature("unit")
 @pytest.allure.story("plugin", "south", "coap")
-def test_plugin_init(mocker):
+def test_plugin_init():
     assert coap.plugin_init(config) == config
 
 
 @pytest.allure.feature("unit")
 @pytest.allure.story("plugin", "south", "coap")
 @pytest.mark.asyncio
-async def test_plugin_start(mocker, unused_port, loop):
+async def test_plugin_start(mocker, unused_port):
     # GIVEN
     port = {
         'description': 'Port to listen on',
@@ -90,71 +92,61 @@ async def test_plugin_start(mocker, unused_port, loop):
     assert coap.aiocoap_ctx is None
 
     # WHEN
-    loop.call_soon(coap.plugin_start(config))
-    await asyncio.sleep(.5)  # required to allow ensure_future task to complete
+    coap.plugin_start(config)
+    await asyncio.sleep(.3)  # required to allow ensure_future task to complete
 
     # THEN
     assert coap.aiocoap_ctx is not None
     assert 1 == log_info.call_count
-    calls = [
-        call('CoAP listener started on port {} with uri {}'.format(config['port']['value'], config['uri']['value']))]
+    calls = [call('CoAP listener started on port {} with uri {}'.format(config['port']['value'], config['uri']['value']))]
     log_info.assert_has_calls(calls, any_order=True)
 
-
-@pytest.allure.feature("unit")
-@pytest.allure.story("plugin", "south", "coap")
-def test_plugin_reconfigure(mocker, unused_port):
-    # GIVEN
-    port = {
-        'description': 'Port to listen on',
-        'type': 'integer',
-        'default': str(unused_port()),
-    }
-    mocker.patch.dict(config, {'port': port})
-    config['port']['value'] = config['port']['default']
-    config['uri']['value'] = config['uri']['default']
-    pstop = mocker.patch.object(coap, '_plugin_stop', return_value=True)
-    log_info = mocker.patch.object(coap._LOGGER, "info")
-
-    # WHEN
-    new_config = coap.plugin_reconfigure(config, _NEW_CONFIG)
-
-    # THEN
-    assert _NEW_CONFIG == new_config
-    assert new_config["restart"] == "yes"
-    assert 2 == log_info.call_count
-    assert 1 == pstop.call_count
-
-
-@pytest.allure.feature("unit")
-@pytest.allure.story("plugin", "south", "coap")
-def test_plugin_reconfigure_else(mocker, unused_port):
-    # GIVEN
-    port = {
-        'description': 'Port to listen on',
-        'type': 'integer',
-        'default': str(unused_port()),
-    }
-    mocker.patch.dict(config, {'port': port})
-    config['port']['value'] = config['port']['default']
-    config['uri']['value'] = config['uri']['default']
-    config2 = copy.deepcopy(config)
-    pstop = mocker.patch.object(coap, '_plugin_stop', return_value=True)
-    log_info = mocker.patch.object(coap._LOGGER, "info")
-
-    # WHEN
-    new_config = coap.plugin_reconfigure(config, config2)
-
-    # THEN
-    assert new_config["restart"] == "no"
-    assert 1 == log_info.call_count
-    assert 0 == pstop.call_count
+    coap.loop.stop()
+    coap.t._tstate_lock = None
+    coap.t._stop()
 
 
 @pytest.allure.feature("unit")
 @pytest.allure.story("plugin", "south", "coap")
 @pytest.mark.asyncio
-async def test_plugin__stop(mocker, unused_port, loop):
+async def test_plugin_reconfigure(mocker, unused_port):
+    # GIVEN
+    port = {
+        'description': 'Port to listen on',
+        'type': 'integer',
+        'default': str(unused_port()),
+    }
+    mocker.patch.dict(config, {'port': port})
+    config['port']['value'] = config['port']['default']
+    config['uri']['value'] = config['uri']['default']
+    new_config = copy.deepcopy(_NEW_CONFIG)
+    new_config['port']['value'] = new_config['port']['default']
+    new_config['uri']['value'] = new_config['uri']['default']
+    log_info = mocker.patch.object(coap._LOGGER, "info")
+
+    # WHEN
+    new_handle = coap.plugin_reconfigure(config, new_config)
+    await asyncio.sleep(.3)  # required to allow ensure_future task to complete
+
+    # THEN
+    assert new_config == new_handle
+
+    # TODO: assert plugin_shutdown, plugin_init, plugin_start called
+    assert 3 == log_info.call_count
+    calls = [call("Old config for CoAP plugin {} \n new config {}".format(config, new_config)),
+             call('Stopping South CoAP plugin...'),
+             call('CoAP listener started on port 1234 with uri sensor-values')]
+    log_info.assert_has_calls(calls, any_order=True)
+
+    coap.loop.stop()
+    coap.t._tstate_lock = None
+    coap.t._stop()
+
+
+@pytest.allure.feature("unit")
+@pytest.allure.story("plugin", "south", "coap")
+@pytest.mark.asyncio
+async def test_plugin_shutdown(mocker, unused_port):
     # GIVEN
     port = {
         'description': 'Port to listen on',
@@ -167,47 +159,21 @@ async def test_plugin__stop(mocker, unused_port, loop):
     log_exception = mocker.patch.object(coap._LOGGER, "exception")
     log_info = mocker.patch.object(coap._LOGGER, "info")
 
-    # WHEN
-    loop.call_soon(coap.plugin_start(config))
-    await asyncio.sleep(.5)  # required to allow ensure_future task to complete
-    coap._plugin_stop(config)
-
-    # THEN
-    assert 2 == log_info.call_count
-    calls = [call('CoAP listener started on port {} with uri sensor-values'.format(config['port']['value'])),
-             call('Stopping South CoAP plugin...')]
-    log_info.assert_has_calls(calls, any_order=True)
-    assert 0 == log_exception.call_count
-
-
-@pytest.allure.feature("unit")
-@pytest.allure.story("plugin", "south", "coap")
-@pytest.mark.asyncio
-async def test_plugin_shutdown(mocker, unused_port, loop):
-    # GIVEN
-    port = {
-        'description': 'Port to listen on',
-        'type': 'integer',
-        'default': str(unused_port()),
-    }
-    mocker.patch.dict(config, {'port': port})
-    config['port']['value'] = config['port']['default']
-    config['uri']['value'] = config['uri']['default']
-    log_exception = mocker.patch.object(coap._LOGGER, "exception")
-    log_info = mocker.patch.object(coap._LOGGER, "info")
+    coap.plugin_start(config)
+    await asyncio.sleep(.3)  # required to allow ensure_future task to complete
 
     # WHEN
-    loop.call_soon(coap.plugin_start(config))
-    await asyncio.sleep(.5)  # required to allow ensure_future task to complete
     coap.plugin_shutdown(config)
 
     # THEN
-    assert 3 == log_info.call_count
-    calls = [call('CoAP listener started on port {} with uri sensor-values'.format(config['port']['value'])),
-             call('Stopping South CoAP plugin...'),
-             call('CoAP plugin shut down.')]
-    log_info.assert_has_calls(calls, any_order=True)
+    assert 2 == log_info.call_count  # includes start call log as well, as a GIVEN condition
+    stop_call = [call('Stopping South CoAP plugin...')]
+    log_info.assert_has_calls(stop_call, any_order=True)
     assert 0 == log_exception.call_count
+
+    coap.loop.stop()
+    coap.t._tstate_lock = None
+    coap.t._stop()
 
 
 @pytest.allure.feature("unit")
@@ -217,7 +183,7 @@ class TestCoapSouthIngest(object):
     """
 
     @pytest.mark.asyncio
-    async def test_render_post_ok(self, loop):
+    async def test_render_post_ok(self):
         data = """{
             "timestamp": "2017-01-02T01:02:03.23232Z-05:00",
             "asset": "sensor1",
@@ -231,19 +197,15 @@ class TestCoapSouthIngest(object):
             }
         }"""
 
-        with patch.object(Ingest, 'increment_discarded_readings', return_value=True) as ingest_discarded:
-            with patch.object(Ingest, 'add_readings', return_value=asyncio.sleep(.1)) as ingest_add_readings:
-                with patch.object(Ingest, 'is_available', return_value=True) as is_ingest_available:
-                    request = message.Message(payload=cbor2.dumps(json.loads(data)), code=numbers.codes.Code.POST)
-                    r = await CoAPIngest.render_post(request)
-                    assert numbers.codes.Code.VALID == r.code
-                    assert '' == r.payload.decode()
-            assert 0 == ingest_discarded.call_count
+        with patch.object(async_ingest, 'ingest_callback') as ingest_add_readings:
+            request = message.Message(payload=cbor2.dumps(json.loads(data)), code=numbers.codes.Code.POST)
+            r = await CoAPIngest.render_post(request)
+            assert numbers.codes.Code.VALID == r.code
+            assert '' == r.payload.decode()
             assert 1 == ingest_add_readings.call_count
-            assert 1 == is_ingest_available.call_count
 
     @pytest.mark.asyncio
-    async def test_render_post_sensor_values_ok(self, loop):
+    async def test_render_post_sensor_values_ok(self):
         data = """{
             "timestamp": "2017-01-02T01:02:03.23232Z-05:00",
             "asset": "sensor1",
@@ -256,44 +218,15 @@ class TestCoapSouthIngest(object):
                 }
             }
         }"""
-        with patch.object(Ingest, 'increment_discarded_readings', return_value=True) as ingest_discarded:
-            with patch.object(Ingest, 'add_readings', return_value=asyncio.sleep(.1)) as ingest_add_readings:
-                with patch.object(Ingest, 'is_available', return_value=True) as is_ingest_available:
-                    request = message.Message(payload=cbor2.dumps(json.loads(data)), code=numbers.codes.Code.POST)
-                    r = await CoAPIngest.render_post(request)
-                    assert numbers.codes.Code.VALID == r.code
-                    assert '' == r.payload.decode()
-            assert 0 == ingest_discarded.call_count
+        with patch.object(async_ingest, 'ingest_callback') as ingest_add_readings:
+            request = message.Message(payload=cbor2.dumps(json.loads(data)), code=numbers.codes.Code.POST)
+            r = await CoAPIngest.render_post(request)
+            assert numbers.codes.Code.VALID == r.code
+            assert '' == r.payload.decode()
             assert 1 == ingest_add_readings.call_count
-            assert 1 == is_ingest_available.call_count
 
     @pytest.mark.asyncio
-    async def test_render_post_is_available_false(self, loop):
-        data = """{
-            "timestamp": "2017-01-02T01:02:03.23232Z-05:00",
-            "asset": "sensor1",
-            "key": "80a43623-ebe5-40d6-8d80-3f892da9b3b4",
-            "readings": {
-                "velocity": "500",
-                "temperature": {
-                    "value": "32",
-                    "unit": "kelvin"
-                }
-            }
-        }"""
-        with patch.object(coap._LOGGER, "exception") as log_exception:
-            with patch.object(Ingest, 'increment_discarded_readings', return_value=True) as ingest_discarded:
-                with patch.object(Ingest, 'is_available', return_value=False) as is_ingest_available:
-                    with pytest.raises(aiocoap.error.ConstructionRenderableError) as excinfo:
-                        request = message.Message(payload=cbor2.dumps(json.loads(data)), code=numbers.codes.Code.POST)
-                        r = await CoAPIngest.render_post(request)
-                    assert str(excinfo).endswith('{"busy": true}')
-                assert 1 == ingest_discarded.call_count
-                assert 1 == is_ingest_available.call_count
-                assert 1 == log_exception.call_count
-
-    @pytest.mark.asyncio
-    async def test_render_post_reading_not_dict(self, loop):
+    async def test_render_post_reading_not_dict(self):
         data = """{
             "timestamp": "2017-01-02T01:02:03.23232Z-05:00",
             "asset": "sensor2",
@@ -301,12 +234,10 @@ class TestCoapSouthIngest(object):
             "readings": "500"
         }"""
         with patch.object(coap._LOGGER, "exception") as log_exception:
-            with patch.object(Ingest, 'increment_discarded_readings', return_value=True) as ingest_discarded:
-                with patch.object(Ingest, 'is_available', return_value=True) as is_ingest_available:
-                    with pytest.raises(aiocoap.error.BadRequest) as excinfo:
-                        request = message.Message(payload=cbor2.dumps(json.loads(data)), code=numbers.codes.Code.POST)
-                        r = await CoAPIngest.render_post(request)
+            with patch.object(async_ingest, 'ingest_callback') as ingest_add_readings:
+                with pytest.raises(aiocoap.error.BadRequest) as excinfo:
+                    request = message.Message(payload=cbor2.dumps(json.loads(data)), code=numbers.codes.Code.POST)
+                    r = await CoAPIngest.render_post(request)
                     assert str(excinfo).endswith('readings must be a dictionary')
-                assert 1 == ingest_discarded.call_count
-                assert 1 == is_ingest_available.call_count
                 assert 1 == log_exception.call_count
+            assert 0 == ingest_add_readings.call_count
